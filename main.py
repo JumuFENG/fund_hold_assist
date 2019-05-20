4f35263dd8c37a4276315903f3e0d4ecedcb8036
zhcbfly <zhcbfly@qq.com>
2019-05-20 21:26:45 +0800

    add main.py to do daily updates.

---
+++ main.py
@@ -0,0 +1,30 @@
+# Python 3
+# -*- coding:utf-8 -*-
+
+from _pwd import db_pwd
+from datetime import datetime, timedelta
+from sql_helper import SqlHelper
+from fund_history import FundHistoryDataDownloader
+from fund_trade import TradeFund
+from commons import *
+
+class DailyUpdater():
+    """for daily update"""
+    def __init__(self):
+        self.dbname = "fund_center"
+        self.sqldb = SqlHelper(password = db_pwd, database = self.dbname)
+
+    def update_all(self):
+        fundcodes = self.sqldb.select(gl_all_info_table, fields=[column_code])
+        fundcodes = [c[0] for c in fundcodes]
+        for c in fundcodes:
+            self.download_all_fund_history(c)
+
+    def download_all_fund_history(self, fundcodes):
+        fh = FundHistoryDataDownloader(fundcodes, dbname = self.dbname, dbpws = db_pwd)
+        fh.fundHistoryTillToday()
+
+if __name__ == '__main__':
+    du = DailyUpdater()
+    #du.download_all_fund_history("161724")
+    du.update_all()
