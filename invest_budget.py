6a569b9b74dbed17afa46832a2cbcca1c2ac8932
zhcbfly <zhcbfly@qq.com>
2019-07-28 17:08:49 +0800

    add min-width of the popup, remove the useless code and copy summary ifles to micloud.

--- invest_budget.py
+++ invest_budget.py
@@ -6,13 +6,13 @@ from datetime import datetime, timedelta
 from decimal import Decimal
 import pandas as pd
 import os
+import shutil
 import json
 
 class InvestBudget():
     """to help do the invest budget"""
     def __init__(self, sqldb):
         self.sqldb = sqldb
-        self.summary_text = ""
 
     def add_budget(self, fund_code, budget, date = ""):
         self.fund_code = fund_code
@@ -59,132 +59,12 @@ class InvestBudget():
 
         self.sqldb.delete(budget_table, {column_consumed:'1'})
 
-    def prepare_sell_info(self, fg, ppg):
-        history_table = fg.history_table
-        history_dvs = self.sqldb.select(history_table, [column_date, column_net_value], order = " ORDER BY %s ASC" % column_date)
-        dvs = history_dvs[-7:]
-        (dateBegin,v) = dvs[0]
-        (d, netvalue) = dvs[-1]
-
-        buy_table = fg.buy_table
-        if not buy_table:
-            return
-        (portion_cannot_sell,), = self.sqldb.select(buy_table, "sum(%s)" % column_portion, "%s > '%s'" % (column_date, dateBegin))
-        dcp_not_sell = self.sqldb.select(buy_table, [column_date, column_cost, column_portion], "%s = 0" % column_soldout)
-        max_value_to_sell = netvalue * (1.0 - float(fg.short_term_rate))
-        dcp_can_sell = []
-        portion_can_sell = 0
-        for (d,c,p) in dcp_not_sell:
-            for (dd,v) in history_dvs:
-                if d == dd and v <= max_value_to_sell:
-                    portion_can_sell += p
-                    dcp_can_sell.append(d)
-        if not portion_cannot_sell:
-            portion_cannot_sell = 0
-        if portion_can_sell == 0:
-            return
-        return "sell: min(" + str(round((fg.portion_hold - portion_cannot_sell) / ppg, 4)) + ", "+ str(round(portion_can_sell/ppg,4)) + ")\n" + str(dcp_can_sell)
-
-    def get_roll_in_info(self, fg, ppg):
-        sell_table = fg.sell_table
-        if not sell_table or not self.sqldb.isExistTable(sell_table):
-            return
-        if not self.sqldb.isExistTableColumn(sell_table, column_rolled_in) or not self.sqldb.isExistTableColumn(sell_table, column_roll_in_value):
-            print("table column not complete.")
-            return
-
-        sell_recs = self.sqldb.select(sell_table, [column_date, column_cost_sold, column_rolled_in, column_roll_in_value])
-        if not sell_recs:
-            return
-
-        rollin_summary = ""
-        for (d, c, r, v) in sell_recs:
-            if not r:
-                r = 0
-            if c <= float(r):
-                continue
-            max_price_to_buy = 0
-            if not v:
-                netvalue = fg.netvalue_by_date(d)
-                max_price_to_buy = round(netvalue * (1.0 - float(fg.short_term_rate)) * ppg, 4)
-            else:
-                max_price_to_buy = round(float(v) * ppg, 4)
-            rollin_summary += d + "<" + str(max_price_to_buy) + ">: " + str(int(c - float(r))) + "\n"
-
-        return rollin_summary.strip()
-
-    def get_budgets(self):
-        if not self.sqldb.isExistTable(gl_fund_info_table):
-            print("can not find fund info DB.")
-            return
-
-        fund_codes = self.sqldb.select(gl_fund_info_table, [column_code])
-
-        no_budget_summary = ""
-        for (c, ) in fund_codes:
-            ppg = 1 if not ppgram.__contains__(c) else ppgram[c]
-            fg = FundGeneral(self.sqldb, c)
-            budget_table = fg.budget_table
-            if budget_table and self.sqldb.isExistTable(budget_table):
-                self.delete_cosumed(budget_table)
-                budget = self.sqldb.select(budget_table, [column_date, column_net_value, column_budget])
-                sum_b = 0
-                index = []
-                values = []
-
-                for (d,v,b) in budget:
-                    sum_b += int(b)
-                    index.append(d)
-                    values.append([Decimal(str(v)) * ppg, b])
-                if sum_b == 0:
-                    no_budget_summary += self.collect_budgets(fg.name, fg.cost_hold, Decimal(str(fg.average)) * ppg)
-                    sell_prepared = self.prepare_sell_info(fg, ppg)
-                    if sell_prepared:
-                        no_budget_summary += sell_prepared + "\n"
-                    rollin_info = self.get_roll_in_info(fg, ppg)
-                    if rollin_info:
-                        no_budget_summary += rollin_info + "\n"
-                else:
-                    self.summary_text += self.collect_budgets(fg.name, fg.cost_hold, Decimal(str(fg.average)) * ppg, index, values, sum_b)
-                    sell_prepared = self.prepare_sell_info(fg, ppg)
-                    if sell_prepared:
-                        self.summary_text += sell_prepared + "\n"
-                    rollin_info = self.get_roll_in_info(fg, ppg)
-                    if rollin_info:
-                        self.summary_text += rollin_info + "\n"
-
-            elif fg.cost_hold and fg.average:
-                no_budget_summary += self.collect_budgets(fg.name, fg.cost_hold, Decimal(str(fg.average)) * ppg)
-                sell_prepared = self.prepare_sell_info(fg, ppg)
-                if sell_prepared:
-                    no_budget_summary += sell_prepared + "\n"
-                rollin_info = self.get_roll_in_info(fg, ppg)
-                if rollin_info:
-                    no_budget_summary += rollin_info + "\n"
-
-        self.summary_text += no_budget_summary
-        self.save_budgets()
-
-    def collect_budgets(self, name, hold, aver_price, index = None, budget = None, budget_sum = 0):
-        summary_text = ""
-        if not hold == 0:
-            summary_text += "\n" + name
-
-        if budget and index and not budget_sum == 0:
-            summary_text += "\nall %d<%.4f> budgets: %d\n" % (hold, aver_price, budget_sum)
-            summary_text += str(pd.DataFrame(data=budget, columns=["net","budget"], index=index)) + "\n"
-        elif not hold == 0:
-            summary_text += "\nall %d<%.4f>\n" % (hold, aver_price)
-
-        return summary_text
-
     def save_budgets(self):
-        f = open(gl_budget_file, 'w')
-        f.write(datetime.now().strftime("%m-%d %H:%M") + self.summary_text)
-        f.close()
-        f = open("budget.txt", 'w')
-        f.write(self.summary_text)
-        f.close()
+        if(not os.path.isdir(gl_summary_dir)):
+            os.mkdir(gl_summary_dir)
+        for parent, dirs, files in os.walk("summary"):
+            for f in files:
+                shutil.copy(os.path.join(parent, f), os.path.join(gl_summary_dir, f))
 
     def manually_update_rolled(self, code, cost, date):
         fg = FundGeneral(self.sqldb, code)
@@ -293,6 +173,8 @@ class InvestBudget():
         f = open("summary/fund.json", 'w')
         f.write("var ftjson = " + json.dumps(fund_json) + ";")
         f.close()
+
+        self.save_budgets()
 
 if __name__ == '__main__':
     dbname = "fund_center"
