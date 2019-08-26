8ba1af57d1cd3725c657d338f01561ff6b5a9d37
chuazhou(svr) <chuazhou@outlook.com>
2019-08-26 20:43:34 +0800

    the server side GET method for /api/get.

--- rest_app/flask_startup.py
+++ rest_app/flask_startup.py
@@ -1,8 +1,13 @@
 # Python 3
 # -*- coding:utf-8 -*-
 
+import sys
+sys.path.append("..")
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from utils import *
+import requests
+import urllib.parse
 
 app = Flask(__name__)
 app.secret_key = "any_string_make_secret_key"
@@ -26,9 +31,25 @@ def login():
 def index():
     return "this is a root for api."
 
+@app.route('/api/get')
+def get_http_request():
+    url = urllib.parse.unquote(request.args.get('url',''))
+    if not url:
+        return "No url specified."
+
+    rsp = requests.get(url)
+    return rsp.content.decode('utf-8')
+
 @app.route('/api/v1/fund/buy', methods=['POST'])
 def add_fund_buy_rec():
     return "OK", 200
+
+@app.route('/api/v1/fund/test', methods=['GET'])
+def test_get():
+    sqldb = SqlHelper(password = db_pwd, database = "fund_center")
+    r = sqldb.select("funds_info", "*", "code=000217")
+    print(r)
+    return str(r)
 
 if __name__ == '__main__':
     app.run(debug=True)
