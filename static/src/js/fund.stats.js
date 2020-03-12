8f90533b0c041b833a69f7f14a464ee7c04ed64e
zhcbfly <zhcbfly@qq.com>
2020-03-12 13:41:26 +0800

    rearrange the stats table.

--- static/src/js/fund.stats.js
+++ static/src/js/fund.stats.js
@@ -41,8 +41,24 @@ class FundStats {
         var tr = document.createElement('tr');
         for (var i = 0; i < hs.length; i++) {
             var th = document.createElement('th');
-            th.setAttribute('onclick', 'fundstats.sortTable(' + i + ')');
+            th.setAttribute('onclick', 'fundstats.sortTable(' + (i + 1) + ')');
             th.appendChild(document.createTextNode(hs[i]));
+            tr.appendChild(th);
+        };
+        return tr;
+    }
+
+    createSpanHeaders(...hs) {
+        var tr = document.createElement('tr');
+        for (var i = 0; i < hs.length; i++) {
+            var th = document.createElement('th');
+            if (hs[i].col && hs[i].col > 1) {
+                th.setAttribute('colspan', hs[i].col);
+            };
+            if (hs[i].row && hs[i].row > 1) {
+                th.setAttribute('rowspan', hs[i].row);
+            };
+            th.appendChild(document.createTextNode(hs[i].name));
             tr.appendChild(th);
         };
         return tr;
@@ -81,24 +97,26 @@ class FundStats {
         };
         
         utils.deleteAllRows(this.statsTable)
-        this.statsTable.appendChild(this.createHeaders('基金名称', '持有成本', '持有收益', '售出成本', '售出额', '次均售出成本', '天数', '卖次数', '总收益', '收益率(%)', '标准收益率(%)', '日均收益', '日均收益率(‱)'));
+
+        this.statsTable.appendChild(this.createSpanHeaders({'name':'基金名称', row:2}, {'name':'持有信息', col:3}, {'name':'售出信息', col:4}, {'name':'收益', col:2}, {'name':'收益率', col:3}));
+        this.statsTable.appendChild(this.createHeaders('成本', '收益', '天数', '总成本', '总额', '次数', '次均', '总', '日均', '次均(%)', '标准(%)', '日均(‱)'));
         for (var i in this.statsJson) {
             this.statsTable.appendChild(utils.createColsRow(
                 this.statsJson[i].name,
                 this.statsJson[i].cost,
                 this.statsJson[i].ewh,
-                this.statsJson[i].cs,
-                this.statsJson[i].acs.toFixed(2),
-                this.statsJson[i].perTimeCostSold.toFixed(2),
                 this.statsJson[i].hds,
+                this.statsJson[i].cs,
+                parseFloat(this.statsJson[i].acs.toFixed(2)),
                 this.statsJson[i].srct,
-                this.statsJson[i].earned.toFixed(2),
-                (this.statsJson[i].earnedRate * 100).toFixed(2),
-                (this.statsJson[i].earnedRate * this.statsJson[i].srct * 100).toFixed(2),
-                this.statsJson[i].perDayEarned.toFixed(2),
-                (this.statsJson[i].perDayEarnedRate * 10000).toFixed(2)));
+                parseFloat(this.statsJson[i].perTimeCostSold.toFixed(2)),
+                parseFloat(this.statsJson[i].earned.toFixed(2)),
+                parseFloat(this.statsJson[i].perDayEarned.toFixed(2)),
+                parseFloat((this.statsJson[i].earnedRate * 100).toFixed(2)),
+                parseFloat((this.statsJson[i].earnedRate * this.statsJson[i].srct * 100).toFixed(2)),
+                parseFloat((this.statsJson[i].perDayEarnedRate * 10000).toFixed(2))));
         };
-        this.lastRow = utils.createColsRow('总计', cost, ewh.toFixed(2), cs, acs.toFixed(2), '-', '-', earned.toFixed(2), (100 * earned / (cost + cs)).toFixed(2), '-', '-', '-');
+        this.lastRow = utils.createColsRow('总计', cost, ewh.toFixed(2), '-', cs, acs.toFixed(2), '-', '-', earned.toFixed(2), '-', (100 * earned / (cost + cs)).toFixed(2), '-', '-');
         this.statsTable.appendChild(this.lastRow);
     }
 
@@ -123,14 +141,14 @@ class FundStats {
 
         var table = this.statsTable;
         var decsort = true;
-        if (this.checkRowsDecreasing(table.rows, n, 1, table.rows.length - 2)) {
+        if (this.checkRowsDecreasing(table.rows, n, 2, table.rows.length - 2)) {
             decsort = false;
         }
 
-        for (var i = 2; i < table.rows.length - 1; i++) {
+        for (var i = 3; i < table.rows.length - 1; i++) {
             var numX = Number(table.rows[i].getElementsByTagName("TD")[n].innerText);
             var shouldSwitch = false;
-            var j = 1;
+            var j = 2;
             for (; j < i; j++) {
                 var numY = Number(table.rows[j].getElementsByTagName("TD")[n].innerText);
                 if (decsort) {
