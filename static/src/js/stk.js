be387957bb9621aacc6091c554f1a38af265f762
zhcbfly <zhcbfly@qq.com>
2020-03-26 17:51:34 +0800

    the function to toggle stockdeatils.

--- static/src/js/stk.js
+++ static/src/js/stk.js
@@ -6,22 +6,49 @@ class StockSummay {
     constructor(code) {
         this.code = code;
         this.container = null;
-        this.header = null;
         this.detail = null;
     }
 
-    initialize() {
+    initialize(hdclick) {
         this.container = document.createElement('div');
-        this.header = document.createElement('div');
+        var header = document.createElement('div');
+        header.onclick = function(e) {
+            if (typeof(hdclick) === 'function') {
+                hdclick();
+            };
+        }
+        this.container.appendChild(header);
+        this.hdname = document.createTextNode('');
+        header.appendChild(this.hdname);
+        this.hdearned = document.createElement('label');
+        header.appendChild(this.hdearned);
+
         this.detail = document.createElement('div');
-        this.container.appendChild(this.header);
+        this.detail.style.display = 'none';
         this.container.appendChild(this.detail);
+        this.container.appendChild(document.createElement('hr'));
+    }
+
+    toggleDetails() {
+        if (this.detail.style.display == "none") {
+            this.detail.style.display = 'block';
+        } else {
+            this.detail.style.display = 'none';
+        }
+    }
+
+    collapseDetails() {
+        if (this.detail.style.display == 'block') {
+            this.detail.style.display = 'none';
+        };
     }
 
     update() {
         var stockData = all_stocks[this.code];
         if (stockData) {
-            this.header.textContent = stockData.name;
+            this.hdname.textContent = stockData.name? stockData.name: this.code;
+            this.hdearned.textContent = 1;
+            this.hdearned.className = 'increase';
             this.detail.textContent = this.code + ' ' + stockData.ptn + ' ' + stockData.avp;
         };
     }
@@ -39,6 +66,7 @@ class StockHub {
         document.body.appendChild(this.container);
         this.topContainer = document.getElementById('top_container');
         this.stockListTable = document.createElement('table');
+        this.stockListTable.appendChild(document.createElement('hr'));
         this.container.appendChild(this.stockListTable);
         trade.fetchStockSummary(null, function(c){
             stockHub.reloadAllStocks();
@@ -88,13 +116,25 @@ class StockHub {
         buyNewArea.appendChild(buyNewBtn);
     }
 
+    toggleStockDetails(code) {
+        for(var i in this.stockSummaryList) {
+            if (this.stockSummaryList[i].code == code) {
+                this.stockSummaryList[i].toggleDetails();
+            } else {
+                this.stockSummaryList[i].collapseDetails();
+            }
+        }
+    }
+
     updateStockSummary(code) {
         var stockSummary = this.stockSummaryList.find(function(s){
             return s.code == code;
         });
         if (!stockSummary) {
             stockSummary = new StockSummay(code);
-            stockSummary.initialize();
+            stockSummary.initialize(function(){
+                stockHub.toggleStockDetails(code);
+            });
             var cell = document.createElement('td');
             cell.appendChild(stockSummary.container);
             var row = document.createElement('tr');
