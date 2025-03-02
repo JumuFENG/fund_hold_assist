93b0f5fcc8ab360688e1ce99e56e308e7af5e52a
chuazhou <chuazhou@outlook.com>
2025-03-02 17:17:24 +0800

    add StockMainFlowSelector

--- pickup/stock_zdf_ranks.py
+++ pickup/stock_zdf_ranks.py
@@ -134,3 +134,179 @@ class StockZdfRanks(StockBaseSelector):
             k.append(f'zdf{n}')
             k.append(f'rank{n}')
         return self.sqldb.select(self.tablename, k, [f'{column_code}="{code}"', f'{column_date} >= "{start}"'])
+
+
+class StockMainFlowSelector(StockBaseSelector):
+    def __init__(self, bks=None):
+        super().__init__(False)
+        self.bks = bks
+
+    def initConstrants(self):
+        super().initConstrants()
+
+    def walk_prepare(self, date=None):
+        self.wkselected = []
+        self.wkstocks = []
+        stocks = []
+        if self.bks is not None:
+            stocks = StockBkMap.bk_stocks(self.bks)
+        else:
+            stocks = StockGlobal.all_stock_codes()
+        stocks = [s for s in stocks if s.startswith('SH') or s.startswith('SZ') or s.startswith('BJ')]
+        sdate = TradingDate.prevTradingDate(TradingDate.maxTradingDate(), 60)
+        sch = StockChangesHistory()
+        self.stock_changes = {}
+        schgs = sch.sqldb.select(sch.tablename, conds=[f'{column_date}>="{sdate}"', f'({column_type}=4 or {column_type}=8193 or {column_type}=8194)'])
+        for x,c,d,t,i in schgs:
+            if c not in stocks: continue
+            if c not in self.stock_changes:
+                self.stock_changes[c] = []
+            self.stock_changes[c].append([c, d.split()[0], t, i])
+        for c, chg in self.stock_changes.items():
+            date0 = chg[0][1]
+            for c, d, t, i in chg:
+                if t == 4 and d > date0:
+                    date0 = d
+            vchg = []
+            for c, d, t, i in chg:
+                if d >= date0:
+                    vchg.append([c,d,t,i])
+            self.stock_changes[c] = vchg
+            self.wkstocks.append([c, date0])
+        sfh = Stock_Fflow_History()
+        self.stock_mainflow = {}
+        s_zd_fd = []
+        for c,d in self.wkstocks:
+            sfh.setCode(c)
+            if sfh._max_date() != TradingDate.maxTradingDate():
+                sfh.getFflowFromEm(c)
+            smf = sfh.dumpMainFlow(c, d)
+            if smf is None or len(smf) == 0:
+                Utils.log(f'no main flow found for {c}, {d}')
+                continue
+            if len([ch for ch in self.stock_changes[c] if ch[2] == 4]) == 0:
+                mxout, modt = smf[0][2], smf[0][1]
+                for s in smf:
+                    if s[2] < mxout:
+                        mxout = s[2]
+                        modt = s[1]
+                smf = [s for s in smf if s[1] > modt]
+                if len(smf) == 0:
+                    continue
+                mxin,mxdt = smf[0][2], smf[0][1]
+                for s in smf:
+                    if s[2] > mxin:
+                        mxin = s[2]
+                        mxdt = s[1]
+                smf = [s for s in smf if s[1]>=mxdt]
+                s_zd_fd.append([c, None, mxdt])
+            else:
+                s_zd_fd.append([c, d, d])
+            self.stock_mainflow[c] = smf
+        self.wkstocks = s_zd_fd
+
+    def walk_on_history_thread(self):
+        while len(self.wkstocks) > 0:
+            c, zd, fd = self.wkstocks.pop(0)
+            bcnt, scnt = 0, 0
+            for chg in self.stock_changes[c]:
+                if zd is not None and chg[1] <= TradingDate.nextTradingDate(zd, 2):
+                    continue
+                if chg[2] == 8193:
+                    bcnt += 1
+                elif chg[2] == 8194:
+                    scnt += 1
+            mf,mfp = 0,0
+            for smf in self.stock_mainflow[c]:
+                mf += smf[2]
+                mfp += smf[7]
+
+            # 3 日表现
+            allkl = self.get_kd_data(c, min(fd, TradingDate.prevTradingDate(TradingDate.maxTradingDate(), 3)), fqt=1)
+            zd3 = round((allkl[-1].close - allkl[-4].close) * 100 / allkl[-4].close, 2)
+            minzd = round(min([(allkl[i].close - allkl[i-1].close) * 100 / allkl[i-1].close for i in range(-3, 0)]), 2)
+            mf3 = 0
+            minmf3 = self.stock_mainflow[c][-1][2]
+            for i in range(1, 4):
+                if len(self.stock_mainflow[c]) > i:
+                    mf3 += self.stock_mainflow[c][-i][2]
+                    if self.stock_mainflow[c][-i][2] < minmf3:
+                        minmf3 = self.stock_mainflow[c][-i][2]
+            self.wkselected.append([c, zd, fd, bcnt, scnt, mf, round(mfp, 2), zd3, minzd, mf3, minmf3])
+
+    def check_stocks(self, stocks):
+        self.wkstocks = [stocks]
+        self.wkselected = []
+        sch = StockChangesHistory()
+        self.stock_changes = {}
+        c, sdate, fdate = stocks
+        schgs = sch.sqldb.select(sch.tablename, conds=[f'{column_date}>="{sdate}"', f'({column_type}=4 or {column_type}=8193 or {column_type}=8194)'])
+        kstocks = [c for c,zd,fd in self.wkstocks]
+        for x,c,d,t,i in schgs:
+            if d < sdate or d > fdate: continue
+            if c not in kstocks: continue
+            if c not in self.stock_changes:
+                self.stock_changes[c] = []
+            self.stock_changes[c].append([c, d.split()[0], t, i])
+        for c, chg in self.stock_changes.items():
+            date0 = chg[0][1]
+            for c, d, t, i in chg:
+                if t == 4 and d > date0:
+                    date0 = d
+            vchg = []
+            for c, d, t, i in chg:
+                if d >= date0:
+                    vchg.append([c,d,t,i])
+            self.stock_changes[c] = vchg
+
+        sfh = Stock_Fflow_History()
+        self.stock_mainflow = {}
+        for c, zd, fd in self.wkstocks:
+            sfh.setCode(c)
+            if sfh._max_date() != TradingDate.maxTradingDate():
+                sfh.getFflowFromEm(c)
+            smf = sfh.dumpMainFlow(c, zd)
+            if smf is None or len(smf) == 0:
+                Utils.log(f'no main flow found for {c}, {zd}')
+                continue
+            if len([ch for ch in self.stock_changes[c] if ch[2] == 4]) == 0:
+                mxout, modt = smf[0][2], smf[0][1]
+                for s in smf:
+                    if s[2] < mxout:
+                        mxout = s[2]
+                        modt = s[1]
+                smf = [s for s in smf if s[1] > modt]
+                if len(smf) == 0:
+                    continue
+                mxin,mxdt = smf[0][2], smf[0][1]
+                for s in smf:
+                    if s[2] > mxin:
+                        mxin = s[2]
+                        mxdt = s[1]
+                smf = [s for s in smf if s[1]>=mxdt and s[1] <= fdate]
+            self.stock_mainflow[c] = smf
+
+        self.walk_on_history_thread()
+        for x in self.wkselected:
+            print(x)
+
+
+    def walk_post_process(self):
+        cands = []
+        self.wkselected = sorted(self.wkselected, key=lambda x: x[5], reverse=True)
+        cands += self.wkselected[0:5]
+        cancodes = [x[0] for x in cands]
+        self.wkselected = sorted(self.wkselected, key=lambda x: x[3]-x[4], reverse=True)
+        for wk in self.wkselected[0:5]:
+            if wk[0] not in cancodes:
+                cands.append(wk)
+
+        cancodes = [x[0] for x in cands]
+        for wk in self.wkselected:
+            if wk[0] in cancodes:
+                continue
+            if wk[7] <= 15 and wk[8] > -3 and wk[9] > 5000000 and wk[10] > 0: # 3 日累计涨幅小于10
+                cands.append(wk)
+
+        for s in cands:
+            print(s)
