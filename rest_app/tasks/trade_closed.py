282dac3a5cc3a199eff3d2d617f9b8586bd55511
chuazhou <chuazhou@outlook.com>
2025-08-15 22:45:59 +0800

    tasks in trade closed.

--- rest_app/tasks/trade_closed.py
+++ rest_app/tasks/trade_closed.py
@@ -5,6 +5,8 @@ import sys
 import os
 sys.path.insert(0, os.path.realpath(os.path.dirname(__file__) + '/../..'))
 
+from traceback import format_exc
+from time import sleep
 from phon.data.user import User
 from phon.data.history import AllStocks, AllIndexes, TradingDate
 from utils import Utils, datetime, shared_cloud_foler
@@ -17,32 +19,61 @@ def save_earning_task():
     if TradingDate.today() != TradingDate.max_trading_date():
         TimerTask.logger.warn(f'today is not trading day!')
         return
-    TimerTask.logger.info(f'trade_closed_task!')
-    User.save_stocks_eaning_html(shared_cloud_foler, [11, 14])
-    dnow = datetime.now()
-    if dnow.weekday() == 4:
-        for uid in [11, 14]:
-            user = User.user_by_id(uid)
-            user.archive_deals(f'{dnow.year + 1}')
+
+    mxretry = 3
+    retry = 0
+    while retry < mxretry:
+        try:
+            TimerTask.logger.info(f'save_earning_task!')
+            User.save_stocks_eaning_html(shared_cloud_foler, [11, 14])
+            dnow = datetime.now()
+            if dnow.weekday() == 4:
+                for uid in [11, 14]:
+                    user = User.user_by_id(uid)
+                    user.archive_deals(f'{dnow.year + 1}')
+            break
+        except Exception as e:
+            retry += 1
+            TimerTask.logger.error(f'Error saving earnings: {e}')
+            TimerTask.logger.error(format_exc())
+            sleep(10)
+            if retry < mxretry:
+                continue
 
 class SaveEarningTask(TimerTask):
     def __init__(self) -> None:
-        super().__init__('15:01:03', save_earning_task)
+        super().__init__('15:02:03', save_earning_task)
 
 
-def update_daily_history_data():
-    AllIndexes.update_kline_data('d')
-    TradingDate.clear_cache()
-    AllStocks.update_kline_data('d')
-    bkchghis = StockBkChangesHistory()
-    bkchghis.updateBkChangedIn5Days()
-    clsbkhis = StockClsBkChangesHistory()
-    clsbkhis.updateBkChangedIn5Days()
+def update_bkchanges_history():
+    try:
+        bkchghis = StockBkChangesHistory()
+        bkchghis.updateBkChangedIn5Days()
+        clsbkhis = StockClsBkChangesHistory()
+        clsbkhis.updateBkChangedIn5Days()
+    except Exception as e:
+        TimerTask.logger.error(f'Error updating bk changes history: {e}')
+        TimerTask.logger.error(format_exc())
+
+
+class BkChagnesHistoryTask(TimerTask):
+    def __init__(self) -> None:
+        super().__init__('15:01:04', update_bkchanges_history)
+
+
+def update_daily_trade_closed_history():
+    try:
+        AllIndexes.update_kline_data('d')
+        TradingDate.clear_cache()
+        AllStocks.update_kline_data('d')
+    except Exception as e:
+        TimerTask.logger.error(f'Error updating daily history data: {e}')
+        TimerTask.logger.error(format_exc())
 
 
 class TradeClosedHistroyDataTask(TimerTask):
     def __init__(self) -> None:
-        super().__init__('15:01:05', update_daily_history_data)
+        super().__init__('15:01:20', update_daily_trade_closed_history)
 
 
 class SmStatsTask1501(TimerTask):
@@ -54,5 +85,5 @@ if __name__ == '__main__':
     Utils.setup_logger()
     TimerTask.setup_logger(Utils.logger)
     TimerTask.logger.info('start trade closed task!')
-    tasks = [SaveEarningTask(), TradeClosedHistroyDataTask(), SmStatsTask1501()]
+    tasks = [SaveEarningTask(), BkChagnesHistoryTask(), SmStatsTask1501(), TradeClosedHistroyDataTask()]
     TimerTask.run_tasks(tasks)
